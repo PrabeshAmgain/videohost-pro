3d0c6da7e8e5bac1f4b1ee176e0ccf5be74ca829
PrabeshAmgain <kind58518@gmail.com>
2025-11-09 17:09:32 +0545

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,27 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    unoptimized: true,
+    domains: ['duuunepzcwbzuxshpomk.supabase.co'],
+  },
+  headers: async () => {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, s-maxage=10, stale-while-revalidate=59',
+          },
+        ],
+      },
+    ];
+  },
+  compress: true,
+  poweredByHeader: false,
+  productionBrowserSourceMaps: false,
+};
+
+module.exports = nextConfig;
